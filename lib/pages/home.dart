2393e6891c59497763a4a6979dbc910da70a7cb2
lionlin1987 <lionlin1987@sina.com>
2018-04-10 22:15:00 +0800

    首页

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("COIN TUBE"), backgroundColor: Colors.blueAccent,),  //头部的标题AppBar
+      appBar: new AppBar(title: new Text("DASHBOARD"), backgroundColor: Colors.blueAccent,),  //头部的标题AppBar
       //侧边栏填充内容
       drawer: new Drawer(
         child: new ListView(
@@ -97,82 +97,101 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: new Container(
+          decoration: new BoxDecoration(    //用一个BoxDecoration装饰器提供背景图片
+            image: new DecorationImage(
+              fit: BoxFit.fill,
+              // image: new NetworkImage('https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg')
+              //可以试试图片调取自本地。调用本地资源，需要到pubspec.yaml中配置文件路径
+              image: new ExactAssetImage('images/lake.jpg'),
+            ),),
         child: new Column(
           children: <Widget>[
-            new Center(
-              child: new Card(
-                child: new Column(
-                  children: <Widget>[
-                    new CircleAvatar(    //圆形图标控件
-                      //图片调取自网络
-                      backgroundImage: new AssetImage('images/lake.jpg'),
+              new Center(
+                child: new Card(
+                  child: new Container(
+                    width: 280.0,
+                    margin: const EdgeInsets.all(20.0),
+                    child: new Column(
+                      children: <Widget>[
+                        new CircleAvatar(    //圆形图标控件
+                          //图片调取自网络
+                          backgroundImage: new AssetImage('images/lake.jpg'),
+                        ),
+                        new Text('Hello John Doe',style: new TextStyle(fontSize: 35.0),),
+                        new Text('last login: 10:33am 2018-04-10',style: new TextStyle(fontSize: 15.0),),
+                      ],
                     ),
-                    new Text('Hello John Doe',style: new TextStyle(fontSize: 35.0),),
-                    new Text('last login: 10:33am 2018-04-10',style: new TextStyle(fontSize: 15.0),),
-                  ],
+                  ),
                 ),
               ),
-            ),
             new Center(
               child: new Card(
                 color: Colors.blue,
-                child: new Column(
-                  children: <Widget>[
-                    new Text('Trading Balance',style: new TextStyle(fontSize: 18.0,color: Colors.white),),
-                    new Text('\$6328.33 AUD',style: new TextStyle(fontSize: 25.0,color: Colors.white),),
-                    new Text('\$590.22',style: new TextStyle(fontSize: 25.0,color: Colors.green),),
-                  ],
+                child: new Container(
+                  width: 280.0,
+                  margin: const EdgeInsets.all(20.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new Text('Trading Balance',style: new TextStyle(fontSize: 18.0,color: Colors.white),),
+                      new Text('\$6328.33 AUD',style: new TextStyle(fontSize: 25.0,color: Colors.white),),
+                      new Text('\$590.22',style: new TextStyle(fontSize: 25.0,color: Colors.green),),
+                    ],
+                  ),
                 ),
               ),
             ),
             new Center(
               child: new Card(
-                child: new Column(
-                  children: <Widget>[
-                    new Text('Cryptocurrency Watchlist',style: new TextStyle(fontSize: 18.0),),
-                    new Container(
-                      margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                      child: new Row(
-                        children: <Widget>[
-                          new CircleAvatar(    //圆形图标控件
-                            //图片调取自网络
-                            backgroundImage: new AssetImage('images/lake.jpg'),
-                          ),
-                          new Text('29 sept.17',style: new TextStyle(fontSize: 18.0),),
-                          new Text('BITCOIN',style: new TextStyle(fontSize: 18.0),),
-                          new Text('0.1800BTC',style: new TextStyle(fontSize: 18.0),),
-                        ],
+                child: new Container(
+                  width: 280.0,
+                  margin: const EdgeInsets.all(10.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new Text('Cryptocurrency Watchlist',style: new TextStyle(fontSize: 18.0),),
+                      new Container(
+                        margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                        child: new Row(
+                          children: <Widget>[
+                            new CircleAvatar(    //圆形图标控件
+                              //图片调取自网络
+                              backgroundImage: new AssetImage('images/lake.jpg'),
+                            ),
+                            new Text('29 sept.17',style: new TextStyle(fontSize: 14.0),),
+                            new Text('BITCOIN',style: new TextStyle(fontSize: 14.0),),
+                            new Text('0.1800BTC',style: new TextStyle(fontSize: 14.0),),
+                          ],
+                        ),
                       ),
-                    ),
-                    new Container(
-                      margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                      child: new Row(
-                        children: <Widget>[
-                          new CircleAvatar(    //圆形图标控件
-                            //图片调取自网络
-                            backgroundImage: new AssetImage('images/lake.jpg'),
-                          ),
-                          new Text('29 sept.17',style: new TextStyle(fontSize: 18.0),),
-                          new Text('BITCOIN',style: new TextStyle(fontSize: 18.0),),
-                          new Text('0.1800BTC',style: new TextStyle(fontSize: 18.0),),
-                        ],
+                      new Container(
+                        margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                        child: new Row(
+                          children: <Widget>[
+                            new CircleAvatar(    //圆形图标控件
+                              //图片调取自网络
+                              backgroundImage: new AssetImage('images/lake.jpg'),
+                            ),
+                            new Text('29 sept.17',style: new TextStyle(fontSize: 14.0),),
+                            new Text('BITCOIN',style: new TextStyle(fontSize: 14.0),),
+                            new Text('0.1800BTC',style: new TextStyle(fontSize: 14.0),),
+                          ],
+                        ),
                       ),
-                    ),
-                    new Container(
-                      margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                      child: new Row(
-                        children: <Widget>[
-                          new CircleAvatar(    //圆形图标控件
-                            //图片调取自网络
-                            backgroundImage: new AssetImage('images/lake.jpg'),
-                          ),
-                          new Text('29 sept.17',style: new TextStyle(fontSize: 18.0),),
-                          new Text('BITCOIN',style: new TextStyle(fontSize: 18.0),),
-                          new Text('0.1800BTC',style: new TextStyle(fontSize: 18.0),),
-                        ],
+                      new Container(
+                        margin: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                        child: new Row(
+                          children: <Widget>[
+                            new CircleAvatar(    //圆形图标控件
+                              //图片调取自网络
+                              backgroundImage: new AssetImage('images/lake.jpg'),
+                            ),
+                            new Text('29 sept.17',style: new TextStyle(fontSize: 14.0),),
+                            new Text('BITCOIN',style: new TextStyle(fontSize: 14.0),),
+                            new Text('0.1800BTC',style: new TextStyle(fontSize: 14.0),),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
